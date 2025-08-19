6d2f475cc5a3f22f2addef43715b4fc15fc96135
Kalana Dharmarathne <kalanaeshan07@gmail.com>
2025-08-19 18:07:14 +0530

    fix the globel.css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
+// app/_layout.tsx
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import "./global.css"; // ✅ This activates Tailwind / NativeWind
 
 export default function RootLayout() {
   return (
